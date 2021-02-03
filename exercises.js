b71eabcbff7eb8394ab7e7f519b386b4391aeade
kchu01 <chukathy01@gmail.com>
2021-02-03 11:56:41 -0800

    Shapes Labwork

--- exercises.js
+++ exercises.js
@@ -8,32 +8,77 @@
 // cheat sheet: the radius of a circle is the distance from the center to the edge. A circle's perimeter is equal to 2 * pi * radius. A circle's area is equal to pi * radius * radius
 // Write your Circle class here
 
+class Circle {
+    constructor(radius) {
+        this.radius = radius,
+            this.perimeter = function () {
+                return Math.PI * this.radius * this.radius
+            }
 
+    }
+}
 
+// const myCircle = new Circle(3)
+// console.log(myCircle.perimeter())
+// console.log('Circle Tests:')
+let myCircle = new Circle(3);
 
+console.log(myCircle.perimeter())
 
 // create a new class called Triangle
 // cheat sheet: a triangle's perimeter is equal to the sum of all its sides. A triangle is a right triangle if (first side)^2 + (second side)^2 === (third side)^2
 // Write your Triangle class here
 
+class Triangle {
+    constructor(side1, side2, side3) {
+        this.side1 = side1,
+            this.side2 = side2,
+            this.side3 = side3,
+            this.perimeter = function () {
+                return this.side1 + this.side2 + this.side3
+            }
+        this.isRightTriangle = function () {
+            return this.side1 * this.side1 + this.side2 * this.side2 === this.side3;
+        }
+    }
+}
 
-
-
+const triangleTest = new Triangle(4, 6, 2)
+// console.log(triangleTest.perimeter())
 
 // create a new class called Rectangle
 // cheat sheet: a rectangle's area is equal to the width times the height. a rectangle's area is equal to the sum of all its sides (drawing a picture might help here)
 // Write your Rectangle class here
 
 
+class Rectangle {
+    constructor(width, height) {
+        this.width = width,
+            this.height = height,
+            this.area = function () {
+                return width * height
+            }
+    }
+}
 
-
+const rectangleTest = new Rectangle(5, 8)
+// console.log(rectangleTest.area())
 
 // create a new class called Square
 // cheat sheet: a square is just a rectangle where the width and the height are the same! the tricky part here is building the right constructor. once that is done, the Rectangle class will do all the work. in other words, you should not have to define a new perimeter and area for the square.
 // Write your Square class here
 
+class Square extends Rectangle {
+    constructor(width, height) {
+        super(width, height);
 
+    }
 
+}
+
+const mySquare = new Square(5, 5)
+
+// console.log(mySquare.perimeter())
 
 
 // ==================
@@ -58,7 +103,7 @@
 // console.log(myTriangle.perimeter() === 13);
 // console.log(myTriangle.isRightTriangle() === false);
 
-// const myRightTriangle = new Triangle(3,4,5);
+// const myRightTriangle = new Triangle(3, 4, 5);
 // console.log(myRightTriangle.side1 === 3);
 // console.log(myRightTriangle.side2 === 4);
 // console.log(myRightTriangle.side3 === 5);
@@ -67,12 +112,12 @@
 
 
 // Rectangle tests:
-// console.log('Rectangle Tests:')
-// const myRectangle = new Rectangle(3, 5);
-// console.log(myRectangle.width === 3);
-// console.log(myRectangle.height === 5);
+console.log('Rectangle Tests:')
+const myRectangle = new Rectangle(3, 5);
+console.log(myRectangle.width === 3);
+console.log(myRectangle.height === 5);
 // console.log(myRectangle.perimeter() === 16);
-// console.log(myRectangle.area() === 15);
+console.log(myRectangle.area() === 15);
 
 
 // Square tests
